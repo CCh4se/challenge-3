482aafe8d03590426a41eb8bf2dacc2143d6e3da
Chase Miller <chasehmiller97@gmail.com>
2023-04-28 00:01:25 -0400

    1st resubmission, but not done

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 // Assignment Code
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numericCharacters = '0123456789';
-var specialCharacters = '!@#$%^&*()';
-var base = [];
+var lowercaseConfirm = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseConfirm = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericConfirm = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialcharactersConfirm = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var password = ""
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var passwordLength = prompt("How many letter do you want in your password")
   console.log(passwordLength);
+
   var lowercaseConfirm = confirm("Do you want lowercase letters in your password")
   console.log(lowercaseConfirm);
+
   var uppercaseConfirm = confirm("Do you want uppercase letters in your password")
   console.log(uppercaseConfirm);
+
   var numericConfirm = confirm("Do you want numeric values in your password")
   console.log(numericConfirm);
+
   var specialcharactersConfirm = confirm("Do you want special characters in your password")
   console.log(specialcharactersConfirm);
+
 
 
   if(passwordLength > 128 || passwordLength < 8){
@@ -44,22 +49,11 @@ function generatePassword() {
   } else {
     base.push(specialCharacters);
   }
-
   return generatePassword;
 }
 
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-   // for (var i = 0; i < passwordLength; i++) {
-     // var generatePassword = Math.floor(Math.random() *(upperCase.length + lowerCase.length));}
-
-  passwordText.value = password;
-
-}
-
-generateBtn.addEventListener("click", writePassword);
+ 
+    
+    
+//event listener for password 
+ generateBtn.addEventListener('click',populatePassword)
